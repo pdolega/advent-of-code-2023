5baf9b248a013490b6562daea38cda1ad8424ab7
pdolega <pdolega@gmail.com>
2023-12-04 18:40:11 +0100

    Day 4 - formatting adjustment

--- lib/day04.ex
+++ lib/day04.ex
@@ -1,8 +1,6 @@
 defmodule Day04 do
   def part1(input) do
-    nums = parse_input(input)
-
-    Enum.map(nums, fn {card_nums, my_nums} ->
+    Enum.map(parse_input(input), fn {card_nums, my_nums} ->
       winning_nums = MapSet.intersection(MapSet.new(card_nums), MapSet.new(my_nums))
       case Enum.count(winning_nums) do
         0 -> 0
@@ -13,22 +11,16 @@ defmodule Day04 do
   end
 
   def part2(input) do
-    scratchcards = parse_input(input)
-    |> Enum.with_index(1)
-    |> Enum.map(fn {scratchard, index} -> {index, scratchard} end)
+    scratchcards = parse_input(input) |> Enum.with_index(1)
+    won_cards = Enum.map(scratchcards, fn {_, index} -> {index, 0} end) |> Enum.into(%{})
 
-    won_cards = Enum.map(scratchcards, fn {index, _} -> {index, 0} end)
-    |> Enum.into(%{})
-
-    new_scratchcards = Enum.reduce(scratchcards, won_cards, fn {index, scratchcard}, won_scratchcards ->
+    new_scratchcards = Enum.reduce(scratchcards, won_cards, fn {scratchcard, index}, won_scratchcards ->
       {card_nums, my_nums} = scratchcard
       winning_nums = MapSet.intersection(MapSet.new(card_nums), MapSet.new(my_nums))
-
       copies_count = Map.get(won_scratchcards, index)
 
       case Enum.count(winning_nums) do
-        0 ->
-          won_scratchcards
+        0 -> won_scratchcards
 
         won_count ->
           Enum.reduce(index+1..index+won_count, won_scratchcards, fn num, new_scratchcards ->
@@ -37,9 +29,7 @@ defmodule Day04 do
       end
     end)
 
-    new_scratchcard_sum = Map.values(new_scratchcards)
-    |> Enum.sum()
-
+    new_scratchcard_sum = Map.values(new_scratchcards) |> Enum.sum()
     new_scratchcard_sum + Enum.count(scratchcards)
   end
 
@@ -51,12 +41,8 @@ defmodule Day04 do
       [nums_on_card, my_nums] = String.split(line, "|")
       [_, nums_on_card] = String.split(nums_on_card, ":")
 
-      nums_left = Regex.scan(nums_regex, nums_on_card)
-      |> Enum.map(fn [_, num] -> String.to_integer(num) end)
-
-      nums_right = Regex.scan(nums_regex, my_nums)
-      |> Enum.map(fn [_, num] -> String.to_integer(num) end)
-
+      nums_left = Regex.scan(nums_regex, nums_on_card) |> Enum.map(fn [_, num] -> String.to_integer(num) end)
+      nums_right = Regex.scan(nums_regex, my_nums) |> Enum.map(fn [_, num] -> String.to_integer(num) end)
       {nums_left, nums_right}
     end)
   end
