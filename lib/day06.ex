7689fb50092bbb6fd644aab82018b5e12cbc7200
pdolega <pdolega@gmail.com>
2023-12-06 07:02:56 +0100

    Day 6 - naive solution

--- lib/day06.ex
+++ lib/day06.ex
@@ -1,15 +1,38 @@
 defmodule Day06 do
   def part1(input) do
-    lines = parse_input(input)
-    :ok
+    [times, distance] = String.split(input, "\n")
+    races = Enum.zip(parse_num_list(times), parse_num_list(distance))
+
+    Enum.map(races, fn {total_time, max_distance} ->
+      count_win_variants(total_time, max_distance)
+    end)
+    |> Enum.reduce(1, &(&1 * &2))
   end
 
   def part2(input) do
-    lines = parse_input(input)
-    :ok
+    [times, distance] = String.split(input, "\n")
+    total_time = parse_big_num(times)
+    max_distance = parse_big_num(distance)
+
+    count_win_variants(total_time, max_distance)
   end
 
-  defp parse_input(input) do
-    String.split(input, "\n")
+  defp count_win_variants(total_time, max_distance) do
+    Enum.filter(0..total_time, fn time_charging ->
+      time_driving = total_time - time_charging
+      distance_driven = time_driving * time_charging
+      distance_driven > max_distance
+    end)
+    |> Enum.count()
+  end
+
+  defp parse_num_list(line) do
+    [_, nums] = String.split(line, ":")
+    String.split(nums, " ") |> Enum.filter(&(&1 != "")) |> Enum.map(&String.trim/1) |> Enum.map(&String.to_integer/1)
+  end
+
+  defp parse_big_num(line) do
+    [_, num] = String.split(line, ":")
+    String.split(num, " ") |> Enum.filter(&(&1 != "")) |> Enum.map(&String.trim/1) |> Enum.join() |> String.to_integer()
   end
 end
--- test/day07_test.exs
+++ test/day07_test.exs
@@ -1,37 +1,38 @@
-import Day06
+import Day07
 
-defmodule Day06Test do
+defmodule Day07Test do
   use ExUnit.Case
   @moduletag timeout: :infinity
 
-  doctest Day06
+  doctest Day07
 
   setup_all do
     test_input =  """
-                  Time:      7  15   30
-                  Distance:  9  40  200\
+
                   """
 
-    real_input = TestUtil.read_input("day06.txt")
+    real_input = TestUtil.read_input("dayXX.txt")
 
     {:ok, %{test_input: test_input, real_input: real_input}}
   end
 
+  @tag :skip
   test "part1 - test input", %{test_input: test_input} do
-    assert part1(test_input) == 288
+    assert part1(test_input) == :ok
   end
 
-
+  @tag :skip
   test "part1 - real input", %{real_input: real_input} do
-    assert part1(real_input) == 2_269_432
+    assert part1(real_input) == :ok
   end
 
+  @tag :skip
   test "part2 - test input", %{test_input: test_input} do
-    assert part2(test_input) == 71_503
+    assert part2(test_input) == :ok
   end
 
   @tag :skip
   test "part2 - real input", %{real_input: real_input} do
-    assert part2(real_input) == 35_865_985
+    assert part2(real_input) == :ok
   end
 end
